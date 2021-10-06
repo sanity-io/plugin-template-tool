a0e7017050479b080a0881792df386883a0b2e96
Fred Carlsen <fred@sjelfull.no>
2021-10-06 13:33:23 +0200

    refactor: migrate to @sanity/ui (#1)

--- template/MyTool.js
+++ template/MyTool.js
@@ -1,19 +1,15 @@
 import React from 'react'
-
-// Sanity uses CSS modules for styling. We import a stylesheet and get an
-// object where the keys matches the class names defined in the CSS file and
-// the values are a unique, generated class name. This allows you to write CSS
-// with only your components in mind without any conflicting class names.
-// See https://github.com/css-modules/css-modules for more info.
-import styles from './MyTool.css'
+import {Box, Text, Stack} from '@sanity/ui'
 
 class MyTool extends React.Component {
   render() {
     return (
-      <div className={styles.container}>
-        <p>This is a blank slate for you to build on.</p>
-        <p>Tools are just React components!</p>
-      </div>
+      <Box padding={4} paddingY={5}>
+        <Stack space={4}>
+          <Text as="p">This is a blank slate for you to build on.</Text>
+          <Text as="p">Tools are just React components!</Text>
+        </Stack>
+      </Box>
     )
   }
 }
